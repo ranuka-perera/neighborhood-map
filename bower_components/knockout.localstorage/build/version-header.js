955104c0fc1084fddc739d1428f7c5ec2fe88b5d
Ranuka Perera <random@sawrc.com>
2015-10-31 10:55:53 +0530

    Update 2015-10-31T05:25:36.383Z

--- bower_components/knockout.localstorage/build/version-header.js
+++
@@ -1,4 +0,0 @@
-// Knockout localStorage plugin v0.0.1
-// (c) 2011 Jim Hoskins - https://github.com/jimrhoskins/knockout.localStorage
-// License: MIT (http://opensource.org/licenses/mit-license)
-
