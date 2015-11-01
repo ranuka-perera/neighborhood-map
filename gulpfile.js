3b6d583e3ca68bc68ebc6f77a65fdabb02075505
Ranuka Perera <ranukap@gmail.com>
2015-11-01 17:34:04 +0530

    * Add typeahead for filter. * Make site responsive. * Add gultfile to upload to github.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    upload_gh = require('gulp-gh-pages');
+
+// Deploy to github gh-pages branch.
+gulp.task('deploy', function () {
+    return gulp.src(['bower_components/*/dist/**/*', 'bower_components/*/build/output/*', 'index.html', 'js/**/*', 'styles/**/*'], {base: './'})
+        //.pipe(gulp.dest('test/'));
+        .pipe(upload_gh());
+});
